03f81b9f9371439151999e8bde0c505b7420c2b3
Imran Khan <imranlove1997@gmail.com>
2022-03-24 11:49:14 +0530

    Client-dashboard

---
+++ src/component/FourOhFour.js
@@ -0,0 +1,7 @@
+const FourOhFour = () => {
+    return (
+        <h1>Not Found</h1>
+    )
+}
+
+export default FourOhFour;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './component/App';
+import Articles from './component/Articles';
+import { BrowserRouter as Router , Route, Switch } from 'react-router-dom';
+import "./style/index.css";
+import Sidebar from './component/Sidebar';
+import Help from './component/Help';
+import Article from './component/Article';
+import FourOhFour from './component/FourOhFour';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Router>
+<div className='container flex'>
+    <Sidebar />
+    <main>
+        <Switch>
+        <Route path='/articles' exact>
+            <Articles />
+        </Route>
+        <Route path='/' exact>
+            <App />
+        </Route>
+        <Route path='/help'>
+            <Help />
+        </Route>
+        <Route path='/articles/:slug'>
+            <Article />
+        </Route>
+        <Route path='*'>
+            <FourOhFour />
+        </Route>
+        </Switch>
+    </main>
+        </div>
+    </Router>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
